c85d0fac42f034e707eb8685c42b26af7d23d83f
Moath Abed <moabed@c1r1s5.42amman.com>
2025-12-28 19:07:50 +0300

    practicing

--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: moabed <moabed@student.42amman.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/27 16:07:52 by moabed            #+#    #+#             */
-/*   Updated: 2025/12/27 17:18:44 by moabed           ###   ########.fr       */
+/*   Updated: 2025/12/28 16:31:33 by moabed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,3 +14,12 @@
 #include "ft_printf/ft_printf.h"
 #include "minilibx-linux/mlx.h"
 #include <sys/time.h>
+
+typedef struct s_data
+{
+    void    *img;
+    char    *addr;
+    int     bits_per_pixel;
+    int     line_length;
+    int     endian;
+} t_data;
--- potato.c
+++ potato.c
@@ -6,13 +6,35 @@
 /*   By: moabed <moabed@student.42amman.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 22:44:02 by moabed            #+#    #+#             */
-/*   Updated: 2025/12/27 17:41:06 by moabed           ###   ########.fr       */
+/*   Updated: 2025/12/28 18:43:53 by moabed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
+void    my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+    char *dst;
+    dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+    *(unsigned int *)dst = color;
+}
+
 int main(void)
 {
-   
+   void *mlx;
+   void *img_win;
+   t_data   img;
+int j = 50;
+int i = 50;
+    mlx = mlx_init();
+    img_win = mlx_new_window(mlx, 1920, 1080 , "prototype");
+    img.img = mlx_new_image(mlx, 1280, 720);
+    img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+    while()
+    {
+        
+    }
+    my_mlx_pixel_put(&img, 50, 50, 0x00FF0000);
+    mlx_put_image_to_window(mlx, img_win,img.img, 0, 0);
+    mlx_loop(mlx);
 }

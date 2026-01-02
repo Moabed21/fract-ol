206df77b57f32774cde0980bada0d46372c39ca7
Moabed21 <moathabed21@gmail.com>
2026-01-02 23:48:54 +0300

    creating the required structs and datatypes

--- fractol.h
+++ fractol.h
@@ -6,20 +6,44 @@
 /*   By: moabed <moabed@student.42amman.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/27 16:07:52 by moabed            #+#    #+#             */
-/*   Updated: 2025/12/28 16:31:33 by moabed           ###   ########.fr       */
+/*   Updated: 2026/01/02 20:58:13 by moabed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft/libft.h"
-#include "ft_printf/ft_printf.h"
-#include "minilibx-linux/mlx.h"
-#include <sys/time.h>
+#ifndef FRACTOL_H
+# define FRACTOL_H
 
-typedef struct s_data
+# include "ft_printf/ft_printf.h"
+# include "libft/libft.h"
+# include "minilibx-linux/mlx.h"
+# include <sys/time.h>
+# define WIDTH 720
+# define HEIGHT 720
+typedef struct s_image
 {
-    void    *img;
-    char    *addr;
-    int     bits_per_pixel;
-    int     line_length;
-    int     endian;
-} t_data;
+	void	*img;
+	char	*pixel_ptr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}			t_image;
+
+typedef struct s_complex
+{
+	double	real;
+	double	imaginary;
+}			t_complex;
+
+typedef struct s_fractal
+{
+	char	*name;
+	void	*mlx_connection;
+	void	*mlx_window; 
+	t_image	img;
+} t_fractal;
+
+//mandlebrot
+void	mandlebrot(t_complex z, t_complex c, int i);
+
+
+#endif

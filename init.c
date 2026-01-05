83fa4e7c854fa60cf11a49b5ad3340379085acdd
Moabed21 <moathabed21@gmail.com>
2026-01-05 20:51:16 +0300

    initializing file done , containting most of the init functions

---
+++ init.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moabed <moabed@student.42amman.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/05 19:41:03 by moabed            #+#    #+#             */
+/*   Updated: 2026/01/05 19:41:55 by moabed           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+void	fractal_init(t_fractal *fractal)
+{
+	fractal->mlx_connection = mlx_init();
+	data_init(fractal);
+	if(fractal->mlx_connection == NULL)
+		malloc_fail();
+	fractal->mlx_window = mlx_new_window(fractal->mlx_connection, WIDTH, HEIGHT, fractal->name);	
+	if(fractal->mlx_window == NULL)
+	{
+		mlx_destroy_display(fractal->mlx_connection);
+		free(fractal->mlx_connection);
+		malloc_fail();
+	}
+	fractal->img.img = mlx_new_image(fractal->mlx_connection, WIDTH, HEIGHT);
+	if(fractal->img.img == NULL)
+	{
+		mlx_destroy_window(fractal->mlx_connection,fractal->mlx_window);
+		mlx_destroy_display(fractal->mlx_connection);
+		free(fractal->mlx_connection);
+		malloc_fail();
+	}
+	fractal->img.pixel_ptr=mlx_get_data_addr(fractal->img.img,
+		&fractal->img.bits_per_pixel, &fractal->img.line_length, &fractal->img.endian);
+}
+void    data_init(t_fractal *fractal)
+{
+    fractal->escape_value= 3;
+    fractal->init_iteration = 1000;
+}
